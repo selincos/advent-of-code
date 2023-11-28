824dda8ba7eb217da9e34513669b19948a3ef15a
Selin Cosar <scosar.cs@gmail.com>
2023-11-28 20:34:07 +0100

    2015 Day01 - Part 2

--- 2015/Day01/solution.py
+++ 2015/Day01/solution.py
@@ -1,14 +1,37 @@
-file = open("input.txt")
-up = 0
-down = 0
 
-for file_obj in file:
-    for line in file_obj:
-        for char in line:
-            if char == '(':
-                up += 1
-            elif char == ')':
-                down += 1
+def part_one():
+    up = 0
+    down = 0
+    file = open("input.txt")
+    for file_obj in file:
+        for line in file_obj:
+            for char in line:
+                if char == '(':
+                    up += 1
+                elif char == ')':
+                    down += 1
 
-floor = up - down
-print("floor: ", floor)
+    floor = up - down
+    print("Santa needs to take the intructions for this floor:", floor)
+
+
+def part_two():
+    up = 0
+    down = 0
+    file = open("input.txt")
+    for file_obj in file:
+        for line in file_obj:
+            for char in line:
+                if char == '(':
+                    up += 1
+                elif char == ')':
+                    down += 1
+
+                floor = up - down
+                if floor == -1:
+                    position = up+down
+                    print("This position of the character that causes Santa to first enter the basement:", position)
+                    return
+
+part_one()
+part_two()
